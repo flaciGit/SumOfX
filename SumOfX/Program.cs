b6ac94c8044f1caf8880e25e5c3a3a2cb94bd3ab
flaciGit <file.laszlo.n@gmail.com>
2019-10-01 11:58:08 +0200

    Update Program.cs

--- SumOfX/Program.cs
+++ SumOfX/Program.cs
@@ -4,18 +4,83 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace SumOfX
+
+namespace SumOfXExample
 {
-    class Program
+    public class SumOfX
     {
+        int[] list;
+
+        SumOfX() {
+            list = new int[] { };
+        }
+
+        public void Append(int[] newList) {
+
+            printArray("\nNew list has been added to the list: ",newList);
+
+            list = list.Concat(newList).ToArray();
+
+        }
+
+        public void printArray(string message, int[] arr) {
+
+            Console.WriteLine(message);
+            for (int i = 0; i < arr.Length; i++)
+            {
+                Console.Write(arr[i] + " ");
+            }
+            Console.Write("\n");
+        }
+        
+        public bool Contains(int sumNumber, int x) {
+
+            printArray("Current list:",list);
+
+            if (x > list.Length) {
+                Console.WriteLine("Given x number: "+ x +" is greater than the current list length! ");
+                return false;
+            }
+
+            int sum = 0;
+            int[] subList;
+            for (int i = 0; i <= list.Length - x; i++) {
+                sum = 0;
+                subList = new int[] { };
+
+                for (int j = i; j < i + x; j++){
+                    sum += list[j];
+                    subList = subList.Concat(new int[] { list[j] }).ToArray();
+                }
+                if (sum == sumNumber)
+                {
+                    Console.WriteLine("The list contains the sum: " + sumNumber + " as " + x + " concurrent sumarization of the numbers!");
+                    printArray("Subset of the list:",subList);
+                    return true;
+                }
+            }
+
+            Console.WriteLine("The list does not contains the sum: " + sumNumber + " as " + x + " concurrent sumarization of the numbers!");
+            return false;
+        }
+
         static void Main(string[] args)
         {
-            // The code provided will print ‘Hello World’ to the console.
-            // Press Ctrl+F5 (or go to Debug > Start Without Debugging) to run your app.
-            Console.WriteLine("Hello World!");
-            Console.ReadKey();
 
-            // Go to http://aka.ms/dotnet-get-started-console to continue learning how to build a console app! 
+            SumOfX sumOfX = new SumOfX();
+
+            sumOfX.Append(new int[] { 1, 2, 3 });
+            sumOfX.Contains(9,4);
+
+            sumOfX.Append(new int[] { 4 });
+            sumOfX.Contains(9, 3);
+
+            sumOfX.Append(new int[] { 4 });
+            sumOfX.Contains(7, 3);
+
+            
+            Console.ReadKey();
+            
         }
     }
 }
